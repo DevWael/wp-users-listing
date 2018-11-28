4b786c53263d30455ae3c63d25c2172b1a9ec419
Ahmad Wael <dev.ahmedwael@gmail.com>
2018-11-28 15:58:49 +0200

    first commit

---
+++ users-listing.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Plugin Name: Users Listing
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: A brief description of the Plugin.
+Version: 1.0
+Author: apple
+Author URI: https://github.com/DevWael
+License: GPLv2
+*/
+
+//Plugin Activation indicator
+if ( ! defined( 'BB_USER_LISTING' ) ) {
+	define( 'BB_USER_LISTING', true );
+}
+
+//plugin version
+if ( ! defined( 'BB_USER_LISTING_VERSION' ) ) {
+	define( 'BB_USER_LISTING_VERSION', 1.0 );
+}
